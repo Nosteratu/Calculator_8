603ab2532c9afa371174106064b22eabd42042b1
Nosteratu <noodc428@gmail.com>
2026-01-06 16:34:35 +0700

    Calculator_8

---
+++ Caculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ForLoopAssignment
+{
+    class NumberCalculator
+    {
+        public void ShowAndSum(int n)
+        {
+            int sum = 0;
+            Console.Write("แสดงตัวเลข: ");
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write(i + " ");
+                sum += i;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("ผลรวม = " + sum);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ป้อนตัวเลข: ");
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            NumberCalculator calculator = new NumberCalculator();
+            calculator.ShowAndSum(inputNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
